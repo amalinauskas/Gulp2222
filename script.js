bb7771f40d263cf6faf02c382c665ca407bed240
amalinauskas <87666505+amalinauskas@users.noreply.github.com>
2021-09-18 17:25:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// antras fonas
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+document.getElementsByClassName('tablinks')[0].click()
+
+// burgeris
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0";
+}
